7f1fed20d8f6f646d28cea76a6eb991e1acb5401
Samuel Osei Kwakye <kwakyesamuelosei@gmail.com>
2019-12-20 08:49:00 +0000

    Update DataSourceService.java

--- src/main/java/io/turntabl/io/datastreamingapp/DataSource/DataSourceService.java
+++ src/main/java/io/turntabl/io/datastreamingapp/DataSource/DataSourceService.java
@@ -45,7 +45,7 @@ public class DataSourceService {
 //            SparkContext sparkContext = new SparkContext(new SparkConf().setAppName("DataSource").set("spark.driver.allowMultipleContexts","true").setMaster("local"));
   
             SparkSession spark = SparkSession.builder().master("local").appName("demo").getOrCreate();
-            spark.conf().set("spark.testing.memory", "2147480000");
+            spark.conf().set("spark.driver.memory", "8g");
             JavaSparkContext javaSparkContext = new JavaSparkContext(spark.sparkContext());
 
             FileStatus[] status = fileSystem.listStatus(new Path("/mydata/"));
@@ -86,7 +86,7 @@ public class DataSourceService {
 //            SparkContext sparkContext = new SparkContext(new SparkConf().setAppName("DataSource").set("spark.driver.allowMultipleContexts","true").setMaster("local"));
             
             SparkSession spark = SparkSession.builder().master("local").appName("demo").getOrCreate();
-            spark.conf().set("spark.testing.memory", "2147480000");
+            spark.conf().set("spark.driver.memory", "8g");
             JavaSparkContext javaSparkContext = new JavaSparkContext(spark.sparkContext());
 
             FileStatus[] status = fileSystem.listStatus(new Path("/mydata/"));
